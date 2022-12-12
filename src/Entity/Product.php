f155db5ecbbf2c081dce324fb2a54278edbc9cd6
PICOUT Tom <79899852+Tommm77@users.noreply.github.com>
2022-12-12 15:24:56 +0100

    ADD Brand and Category entites

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -55,6 +55,12 @@ class Product
     #[ORM\OneToMany(mappedBy: 'product', targetEntity: CartsProducts::class)]
     private Collection $cartsProducts;
 
+    #[ORM\ManyToOne(inversedBy: 'product')]
+    private ?Category $category = null;
+
+    #[ORM\ManyToOne(inversedBy: 'product')]
+    private ?Brand $brand = null;
+
     public function __construct()
     {
         $this->users = new ArrayCollection();
@@ -251,6 +257,30 @@ class Product
                 $cartsProduct->setProduct(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getBrand(): ?Brand
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?Brand $brand): self
+    {
+        $this->brand = $brand;
 
         return $this;
     }
