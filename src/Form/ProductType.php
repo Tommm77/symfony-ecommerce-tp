4b1b135e325049c4e9050ed61aff45001afa86a5
PICOUT Tom <79899852+Tommm77@users.noreply.github.com>
2022-12-13 09:41:36 +0100

    ADD Controller, Form and CRUD

---
+++ src/Form/ProductType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('excerpt')
+            ->add('description')
+            ->add('image')
+            ->add('quantity')
+            ->add('sold')
+            ->add('price')
+            ->add('statut')
+            ->add('createdAt')
+            ->add('updateAt')
+            ->add('seller')
+            ->add('users')
+            ->add('category')
+            ->add('brand')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
