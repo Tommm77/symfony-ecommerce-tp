f1bda04132c79454faaaeb19e1aa9a72adc14b6a
PICOUT Tom <79899852+Tommm77@users.noreply.github.com>
2022-12-14 10:37:03 +0100

    📦 NEW:  Pagination

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProductRepository;
 use App\Entity\Product;
 use App\Form\ProductType;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class ContentController extends AbstractController
@@ -30,10 +31,12 @@ class ContentController extends AbstractController
     }
 
     #[Route('/shop', name: 'app_shop', methods: ['GET'])]
-    public function showAllShop(ProductRepository $productRepository): Response
+    public function showAllShop(ProductRepository $productRepository, PaginatorInterface $paginator, Request $request): Response
     {
         return $this->render('content/shop.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $paginator->paginate($productRepository->findAll(),
+            $request->query->getInt('page', 1),6
+        ),
         ]);
     }
 
